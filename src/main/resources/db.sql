199e6d44fd884b4e8683bbca9192a998cd320e51
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-03-25 13:34:48 +0700

    Update

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,6 +1,6 @@
-DROP TABLE student;
+DROP TABLE IF EXISTS student;
 
-CREATE TABLE IF NOT EXISTS student(
+CREATE TABLE student(
     id   INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(20) NOT NULL,
     age  INT NOT NULL,
