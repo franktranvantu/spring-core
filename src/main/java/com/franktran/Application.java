173ed4a2f3af6c8ed2248584816a897e48501845
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-03-25 10:46:46 +0700

    Update

--- src/main/java/com/franktran/Application.java
+++ src/main/java/com/franktran/Application.java
@@ -1,21 +1,15 @@
 package com.franktran;
 
-import com.franktran.config.JdbcConfig;
 import com.franktran.dao.StudentDao;
-import com.franktran.mapper.StudentMapper;
 import com.franktran.model.Student;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-import java.util.List;
-
+@ComponentScan
 public class Application {
 
   public static void main(String[] args) {
-    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-    context.register(JdbcConfig.class);
-    context.register(StudentDao.class);
-    context.register(StudentMapper.class);
-    context.refresh();
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
 
     StudentDao studentDao = context.getBean("studentDao", StudentDao.class);
     studentDao.dropTableStudent();
