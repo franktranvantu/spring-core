88efb3d097ba02bd37050e25f0ce84794a4a9789
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-03-26 14:31:22 +0700

    Update

--- src/main/java/com/franktran/config/WebMvcConfig.java
+++ src/main/java/com/franktran/config/WebMvcConfig.java
@@ -1,13 +1,10 @@
 package com.franktran.config;
 
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -24,21 +21,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
         resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".jsp");
         return resolver;
-    }
-//
-//    @Bean
-//    public MessageSource messageSource() {
-//        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
-//        source.setBasename("messages");
-//        return source;
-//    }
-
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/resources/**")
-                .addResourceLocations("/WEB-INF/static/");
     }
 
     @Override
--- src/main/webapp/WEB-INF/views/header.jsp
+++
@@ -1,9 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>Header</title>
-</head>
-<body>
-    <p>The Header</p>
-</body>
-</html>
