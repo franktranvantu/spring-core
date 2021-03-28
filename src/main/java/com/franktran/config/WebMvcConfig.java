668217c9fb53a50ee91ed43586268bf2d9d2b9e8
Frank Tran <dss_pm_se1@vtijs.com>
2021-03-28 17:32:56 +0700

    Update

--- src/main/java/com/franktran/config/WebMvcConfig.java
+++ src/main/java/com/franktran/config/WebMvcConfig.java
@@ -22,9 +22,4 @@ public class WebMvcConfig implements WebMvcConfigurer {
         resolver.setSuffix(".jsp");
         return resolver;
     }
-
-    @Override
-    public Validator getValidator() {
-        return null;
-    }
 }
