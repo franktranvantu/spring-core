9e2349e0d803f644d12953c8d816fa015f791354
Frank Tran <dss_pm_se1@vtijs.com>
2021-03-28 17:21:10 +0700

    Update

---
+++ web-bean-scopes/src/main/java/com/franktran/web/model/MyBean.java
@@ -0,0 +1,14 @@
+package com.franktran.web.model;
+
+public class MyBean {
+
+  private String websiteName = "franktran.com";
+
+  public String getWebsiteName() {
+    return websiteName;
+  }
+
+  public void setWebsiteName(String websiteName) {
+    this.websiteName = websiteName;
+  }
+}
