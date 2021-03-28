65e0bd9646a457f9ae8fdcfefeaf15815cf291f3
Frank Tran <dss_pm_se1@vtijs.com>
2021-03-28 17:30:29 +0700

    Update

--- web-bean-scopes/src/main/java/com/franktran/web/config/WebMvcConfig.java
+++ web-bean-scopes/src/main/java/com/franktran/web/config/WebMvcConfig.java
@@ -2,9 +2,11 @@ package com.franktran.web.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "com.franktran.web")
 public class WebMvcConfig implements WebMvcConfigurer {
 }
