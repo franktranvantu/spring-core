9843326f2e409ad9d27b13a888e0fe4fd1b8f308
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-03-30 16:54:14 +0700

    Update

---
+++ src/main/java/com/franktran/lc/UserRegistrationDTO.java
@@ -0,0 +1,59 @@
+package com.franktran.lc;
+
+public class UserRegistrationDTO {
+
+    private String name;
+    private String username;
+    private char[] password;
+    private String countryName;
+    private String[] hobbies;
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public char[] getPassword() {
+        return password;
+    }
+
+    public void setPassword(char[] password) {
+        this.password = password;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String[] getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String[] hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/lc/registration-success.jsp
@@ -0,0 +1,23 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Registration Success</title>
+</head>
+<body>
+    <h1>You registered successful!</h1>
+    <h2>The details you entered are:</h2>
+    <p>Name: ${userRegistration.name}</p>
+    <p>Username: ${userRegistration.username}</p>
+    <p>Country: ${userRegistration.countryName}</p>
+    <p>
+        Hobbies:
+        <ul>
+            <c:forEach var="hobby" items="${userRegistration.hobbies}">
+                <li>${hobby}</li>
+            </c:forEach>
+        </ul>
+    </p>
+    <p>Gender: ${userRegistration.gender}</p>
+</body>
+</html>
