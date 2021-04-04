2d12cc916d80eba24da9d517ea46d7edcc390c14
Frank Tran <dss_pm_se1@vtijs.com>
2021-04-04 08:59:45 +0700

    Update

--- src/main/java/com/franktran/mvc/controller/UserController.java
+++ src/main/java/com/franktran/mvc/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
     @GetMapping("/")
     public String userForm(Model model) {
         model.addAttribute("users", userService.list());
-        return "mvc/users";
+        return "users";
     }
 
     @ModelAttribute("user")
@@ -35,7 +35,7 @@ public class UserController {
 
         if (result.hasErrors()) {
             model.addAttribute("users", userService.list());
-            return "mvc/users";
+            return "users";
         }
 
         userService.save(user);
@@ -48,7 +48,7 @@ public class UserController {
 
         if (result.hasErrors()) {
             model.addAttribute("users", userService.list());
-            return "mvc/users";
+            return "users";
         }
 
         userService.save(user);
